d83858d767e0cd706ec6c07c660d875bab4ed14c
karan <[kiran.magespider@gmail.com]>
2020-10-16 17:30:22 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import './screens/product_detail_screen.dart';
+import './screens/products_overview_screen.dart';
+import './provider/products_provider.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (ctx) => ProductsProvider(),
+      // value: ProductsProvider(),
+      child: MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrange,
+          fontFamily: 'Lato',
+        ),
+        // home: ProductsOverviewScreen(),
+        routes: {
+          '/': (ctx) => ProductsOverviewScreen(),
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+        },
+      ),
+    );
+  }
+}
---
+++ lib/screens/products_overview_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/products_grid.dart';
+
+class ProductsOverviewScreen extends StatelessWidget {
+  static const routeName = '/';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MyShop'),
+      ),
+      body: ProductsGrid(),
+    );
+  }
+}
