01cf22bf2a7f0d3e2146ca272efb0d20b7ce0fcb
karan <[kiran.magespider@gmail.com]>
2020-10-21 10:50:35 +0530

    order screen

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -5,6 +5,7 @@ import '../screens/cart_screen.dart';
 import '../provider/cart.dart';
 import '../widgets/badge.dart';
 import '../widgets/products_grid.dart';
+import '../widgets/app_drawer.dart';
 
 enum FilterOptions {
   Favorites,
@@ -71,6 +72,7 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
           )
         ],
       ),
+      drawer: AppDrawer(),
       body: ProductsGrid(_showOnlyFavorites),
     );
   }
